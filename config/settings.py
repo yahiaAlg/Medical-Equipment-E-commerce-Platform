69f3ed92fcb59888c6df3ae1c88dc75e55d7896f
yahiaAlg <yahialinus21alg@gmail.com>
2026-01-01 21:36:52 +0100

    `Added dotenv and psycopg2-binary dependencies and loaded environment variables from .env file`

--- config/settings.py
+++ config/settings.py
@@ -1,5 +1,9 @@
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
