c1a08f846a746f3c1984fd4b4d5b63712b306cd9
yahiaAlg <yahialinus21alg@gmail.com>
2026-01-02 16:23:37 +0100

    `Added default value to ALLOWED_HOSTS`

--- config/settings.py
+++ config/settings.py
@@ -13,7 +13,9 @@ SECRET_KEY = os.environ.get(
 
 DEBUG = os.environ.get("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",") or [
+    "*"
+]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
